4fc78b0ed3e13dc03d324b2bf4651dffc30f7851
Gabriel Paiva Sousa <gabriel.paiva@take.net>
2021-09-15 20:37:43 -0300

    Got container running

--- src/transactions/entities/transaction.entity.ts
+++ src/transactions/entities/transaction.entity.ts
@@ -1,1 +1,36 @@
-export class Transaction {}
+import {
+  Model,
+  Column,
+  Table,
+  PrimaryKey,
+  DataType,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'transactions',
+  createdAt: 'created_at',
+  updatedAt: 'updated_at',
+})
+export class Transaction extends Model {
+  @PrimaryKey
+  @Column({ type: DataType.UUID, defaultValue: DataType.UUIDV4 })
+  id: string;
+
+  @Column({ allowNull: false })
+  payment_date: Date;
+
+  @Column({ allowNull: false })
+  name: string;
+
+  @Column({ allowNull: false })
+  description: string;
+
+  @Column({ allowNull: false })
+  category: string;
+
+  @Column({ allowNull: false })
+  amount: number;
+
+  @Column({ allowNull: false })
+  type: string;
+}
